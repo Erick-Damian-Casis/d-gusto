3d62601e24c31960dce13756d8fee91cc07fa675
1727003855 <ejc.damian@yavirac.edu.ec>
2022-09-07 11:24:01 -0500

    first commit

---
+++ database/migrations/2022_09_04_073718_create_foods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('name');
+            $table->decimal('cost', $precision = 3, $scale = 2);
+            $table->boolean('bool');
+            $table->boolean('special');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}
