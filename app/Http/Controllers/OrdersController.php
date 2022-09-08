bd18c43a742122a41ca37cf44b7fc7474907a7da
1727003855 <ejc.damian@yavirac.edu.ec>
2022-09-07 19:50:53 -0500

    complete order controller

---
+++ app/Http/Controllers/OrdersController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Food;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrdersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $orders = Order::get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $order = new Order();
+        $order->food()->associate(Food::find($request->input('food.id')));
+        $order->spec = $request->input('spec');
+        $order->amount = $request->input('amount');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Order $order)
+    {
+        $order->food()->associate(Food::find($request->input('food.id')));
+        $order->spec = $request->input('spec');
+        $order->amount = $request->input('amount');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Order $order)
+    {
+        $order->delete();
+    }
+}
